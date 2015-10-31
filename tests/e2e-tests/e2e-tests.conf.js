3a8bc787be3df505075d01936de4ed18ec6c4590
Alexander Pan <alexanderdavidpan@gmail.com>
2015-10-30 23:45:31 -0700

    Setup basic config file for end to end testing
    - Use chrome with disabled web security to make HTTP requests to server
    - Specify localhost:8100 as baseUrl
    - Direct testing to correct file
    - Use verbose logging so the executed test is displayed
    - Use jasemine2 framework

---
+++ tests/e2e-tests/e2e-tests.conf.js
@@ -0,0 +1,16 @@
+exports.config = {  
+        capabilities: {
+            'browserName': 'chrome',
+            'chromeOptions': {                
+                args: ['--disable-web-security']
+            } 
+        },
+        baseUrl: 'http://localhost:8100',
+        framework: 'jasmine2',
+        specs: [
+            '*.tests.js'
+        ],
+        jasmineNodeOpts: {
+            isVerbose: true,
+        }
+};
